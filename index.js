a339a2c4ccbb0e3836e7c0fb2a2cdb0c3fac5123
ozziexyz <ozzieh@skiff.com>
2023-06-28 11:43:56 -0400

    basic file serving

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-import fs from "fs"
-import http from "http"
+import fs from "fs";
+import http from "http";
+import querystring from "querystring";
+
+
 
 const server = http.createServer((req, res) => {
-	res.write("echhs wiki");
-	res.end();
+	var file;
+
+	if (req.path) {
+		file = `web/${req.path}.html`;
+	}
+	else {
+		
+		file = "web/index.html";
+	}
+
+	fs.readFile("web/index.html", (err, data) => {
+		if (err) res.write("Not Found").end();
+		else {
+			res.writeHead(200, {
+				"Content-Length": Buffer.byteLength(data),
+				"Content-Type": "text/html"
+			});
+		}
+
+		res.end(data);
+	});
 });
 
 server.listen(8080);
