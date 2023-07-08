961b12c2a4473fda0d306e57b8da0141c73d0611
ozziexyz <ozzieh@skiff.com>
2023-07-08 17:30:46 -0400

    basic frontend article loader

---
+++ web/src/scripts/article.js
@@ -0,0 +1,5 @@
+var article = { // stores info about the article
+    title: location.pathname.replace("/wiki/", "").replace(".html", "")
+}
+
+document.querySelectorAll(".nav-link")[2].href = `https://github.com/echhs/wiki/commits/main/web/wiki/${article.title}.html`;
